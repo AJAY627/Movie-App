79b078d5650d7f5ab03fcceec19d7e48c3287fc7
AJAY627 <ajay41527@gmail.com>
2021-07-30 16:26:06 +0530

    action creators

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
- import { data } from '../data';
+import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies } from '../actions';
 
 class App extends React.Component {
   componentDidMount() {
@@ -12,32 +13,29 @@ class App extends React.Component {
     });
     //make api call
     //dispatch action
-    store.dispatch({
-      type:'ADD_MOVIES',
-      movies:data
-    });
+    store.dispatch(addMovies(data));
     console.log('STATE', this.props.store.getState());
   }
   render() {
-  const movies = this.props.store.getState();
-  console.log('RENDER');
-  return (
-    <div className="App">
-     <Navbar/>
-     <div className="main"> 
-       <div className="tabs">
-         <div className="tab">Movies</div>
-         <div className="tab">Favourites</div>
-       </div>
+    const movies = this.props.store.getState();
+    console.log('RENDER');
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
 
-       <div className="list">
-         {movies.map((movie, index) => (
-           <MovieCard movie={movie} key={`movies-${index}`} />
-         ))}
+          <div className="list">
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+          </div>
         </div>
-     </div>
-    </div>
-  );
+      </div>
+    );
   }
 }
 
