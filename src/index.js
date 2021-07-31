7109937e4616347ef97327761e8fe984287385b3
AJAY627 <ajay41527@gmail.com>
2021-07-31 19:13:53 +0530

    add middlewere using curried concept

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware  } from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer);
+//use Currying cncept
+//function logger(obk, next, action)
+//logger(obj)(next)(action)
+const logger = function( { dispatch, getState }){
+    return function (next) {
+        return function(action) {
+            //middleware code 
+            console.log('ACTION_TYPE=',action.type);  
+            next(action);        
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('store',store);
 // console.log('SBEFORE STATE', store.getState());
 
