58fd5ba69bf5e7d860cd2459d945bbe7ace6140b
AJAY627 <ajay41527@gmail.com>
2021-07-31 14:30:08 +0530

    add favourite movies

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,12 +8,18 @@
 
 // action types
 export const ADD_MOVIES = 'ADD_MOVIES';
-
+export const ADD_FAVOURITE = 'ADD_FAVOURITE';
 
 //action creaters
-export function addMovies (movies) {
+export function addMovies(movies) {
     return {
         type:ADD_MOVIES,
         movies:movies
+    }
+}
+export function addFavourite(movie) {
+    return {
+        type: ADD_FAVOURITE,
+        movie:movie
     }
 }
